fa5a7fd2f1b2331128ebb30f58d633424b135f2c
rhowell476 <rhowell91@live.com>
2023-01-27 10:17:44 -0600

    1-27

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
 //-------------------SITE HEADER AND FOOTER------------------------//
 //-------SITE HEADER-------//
 
-const header = (function(){//Create site header and navigation elements
-    const siteHeader = document.createElement("header");
-    const headerNav = document.createElement("nav");
-    const headerUL = document.createElement("ul");
+const header = (function(){
     
     //map a navigation link list. key: name, value: page.html
     const navItems = new Map();
     navItems.set('Home', 'index.html');
     navItems.set('Pages', 'pages.html');
+    
+    //TEST
+    const siteHeaderFrag = document.createDocumentFragment();
+    const siteHeader = siteHeaderFrag
+      .appendChild(document.createElement('header'))
+      .appendChild(document.createElement('nav'))
+      .appendChild(document.createElement('ul'));
+
+    document.body.prepend(siteHeaderFrag);
 
     navItems.forEach((key, value) => {
         const navListItems = document.createElement("li");
         const navListLinks = document.createElement("a");
-        navListLinks.setAttribute('href', `/${key}`);
+        navListLinks.setAttribute('href', `/RandomWebBits/${key}`);
         navListLinks.textContent = `${value}`;
         navListItems.prepend(navListLinks);
-        headerUL.append(navListItems);
+        siteHeader.append(navListItems);
     });
-    headerNav.append(headerUL);
-    siteHeader.append(headerNav);
+    const main = document.querySelector("main");
+    main.prepend(siteHeaderFrag);
 
-    //Create site H1
+    //Create Random Web Bits H1
     const H1 = document.createElement("H1");
     H1.textContent = '<Random Web Bits>';
-    H1.setAttribute("id", "ArbitraryWebBits");
+    H1.setAttribute("id", "RandomWebBits");
+    document.querySelector("header").append(H1);
 
-    siteHeader.append(H1);
-
-    // Add site header and H1 to DOM
-    document.querySelector("body").prepend(siteHeader);
 })();
     
 const footer = (function(){
     //-------------------SITE FOOTER------------------------//
     //-------SITE FOOTER-------//
-    const siteFooter = document.createElement("footer")
-    const footerPara = document.createElement("p")
+    const siteFooter = document.createElement("footer");
+    const footerPara = document.createElement("p");
     footerPara.textContent = `\u00A9 2022 Random WebBits. All Rights Reserved.`;
     siteFooter.append(footerPara);
     
+    //Designed by IconHome 
+    const footerIconPara = document.createElement("p");
+    const footerIconLink = document.createElement("a");
+    footerIconLink.href = 'https://www.vectorstock.com/royalty-free-vector/maintenance-icon-for-graphic-and-web-design-vector-45026755'
+    footerIconLink.setAttribute('title', "VectorStock");
+    footerIconLink.setAttribute('target', "_blank");
+    footerIconLink.textContent = '(Image #45026755 at VectorStock.com)';
+    footerIconPara.textContent = `Favicon icon designed by IconHome `;
+    footerIconPara.appendChild(footerIconLink);
+    siteFooter.append(footerIconPara);
+
     //add site footer to DOM
     document.querySelector("body").append(siteFooter);
 })();

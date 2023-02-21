53cfe0cb67c784557248a9d1dba21591f5c102fb
rhowell476 <rhowell91@live.com>
2023-02-21 11:51:05 -0600

    ToDo List Component
    Delete dictionary.js
    
    Update event listener
    
    Input validation
    
    Input validation
    
    ToDo List Component
    
    ToDo List Component
    
    ToDo List Component
    
    ToDo List Component
    
    Update README.md
    
    Update README.md
    
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    Update README.md
    
    Add files via upload
    
    Add files via upload
    
    Update README.md

--- js/WebBits.js
+++ js/WebBits.js
@@ -2,60 +2,63 @@
 import ArbitraryArticles from "./components/data.js";
 import LinkDetails from "./script.js";
 
-const cardTiles = (() => {
+(() => {
 
+    window.addEventListener("DOMContentLoaded", () => {
+        cardsWidget.init();
+    })
+
+    //Icon links used for image Attribution
     class IconLink extends LinkDetails {
         constructor(pageName, hReference, title, innerText, owner, id) {
             super(title, innerText);
             this.pageName = pageName,
-            this.hReference = hReference,
-            this.owner = owner,
-            this.id = id
+                this.hReference = hReference,
+                this.owner = owner,
+                this.id = id
         }
     }
-    
-    //Icon links
+
+    const domainLookupIconLink = new IconLink(
+        "Domain Lookup",
+        "https://www.flaticon.com/free-icons/domain",
+        "domain icons",
+        "Domain icons created by Freepik - Flaticon",
+        "Flaticon",
+        1
+    );
     const htmlFramesIconLink = new IconLink(
         "HTML Frames",
-         "https://www.flaticon.com/free-icons/html",
-         "html icons",
-         "Html icons created by Freepik - Flaticon",
-         "Flaticon",
-         2
-     );
-     const httpsCertIconLink = new IconLink(
-         "HTTPS Certificate",
-         "https://www.flaticon.com/free-icons/ssl-certificate",
-         "ssl certificate icons",
-         "Ssl certificate icons created by inipagistudio - Flaticon",
-         "Flaticon",
-         4
-     );
-     const domainLookupIconLink = new IconLink(
-         "Domain Lookup",
-         "https://www.flaticon.com/free-icons/domain",
-         "domain icons",
-         "Domain icons created by Freepik - Flaticon",
-         "Flaticon",
-         1
-     );
-     const aiIconLink = new IconLink(
-         "Preview chatGPT",
-         "https://www.flaticon.com/free-icons/ai",
-         "ai icons",
-         "Ai icons created by Freepik - Flaticon",
-         "Flaticon",
-         8
-     );
-     const prototypeIconLink = new IconLink(
-         "Paint 3D",
-         "https://www.flaticon.com/free-icons/prototype",
-         "prototype icons",
-
-         "Prototype icons created by Freepik - Flaticon",
-         "Flaticon",
-         9
-     );
+        "https://www.flaticon.com/free-icons/html",
+        "html icons",
+        "Html icons created by Freepik - Flaticon",
+        "Flaticon",
+        2
+    );
+    const httpsCertIconLink = new IconLink(
+        "HTTPS Certificate",
+        "https://www.flaticon.com/free-icons/ssl-certificate",
+        "ssl certificate icons",
+        "Ssl certificate icons created by inipagistudio - Flaticon",
+        "Flaticon",
+        4
+    );
+    const aiIconLink = new IconLink(
+        "Preview chatGPT",
+        "https://www.flaticon.com/free-icons/ai",
+        "ai icons",
+        "Ai icons created by Freepik - Flaticon",
+        "Flaticon",
+        8
+    );
+    const prototypeIconLink = new IconLink(
+        "Paint 3D",
+        "https://www.flaticon.com/free-icons/prototype",
+        "prototype icons",
+        "Prototype icons created by Freepik - Flaticon",
+        "Flaticon",
+        9
+    );
     const dictionaryIconLink = new IconLink(
         "Dictionary Terms",
         "https://www.flaticon.com/free-icons/dictionary",
@@ -72,7 +75,7 @@ const cardTiles = (() => {
         "BOINC",
         11
     );
-     const ipIconLink = new IconLink(
+    const ipIconLink = new IconLink(
         "IP Address Lookup",
         "https://www.flaticon.com/free-icons/ip",
         "IP icons",
@@ -80,102 +83,108 @@ const cardTiles = (() => {
         "Flaticon",
         12
     );
-     const FLATICONS = [htmlFramesIconLink, httpsCertIconLink, domainLookupIconLink,
-         aiIconLink, prototypeIconLink, dictionaryIconLink, boincIconLink, 
-         ipIconLink
-     ];
-    
-    //Create Artibrary Articles section element and append to Main
-    const pageMain = document.querySelector("main");
-    const AASection = document.createElement("section");
-    AASection.classList.add("cards");
-    AASection.classList.add("dictionaryWidget");
+    const FLATICONS = [htmlFramesIconLink, httpsCertIconLink, domainLookupIconLink,
+        aiIconLink, prototypeIconLink, dictionaryIconLink, boincIconLink,
+        ipIconLink
+    ];
 
-        //create card section heading and div element. Append to section
-    let aaHeading = document.createElement('h2');
-    aaHeading.innerText = `Arbitrary Articles:`;
-    let aaCardsSection = document.createElement('div');
-    aaCardsSection.classList.add('card_columns');
-    AASection.appendChild(aaHeading);
-    AASection.appendChild(aaCardsSection);
-    pageMain.append(AASection);
+    const cardsWidget = {
+        init: () => {
+            let WebBits = cardsWidget.buildWebBitsArticleCards();
+            let cardsSection = cardsWidget.buildCardSection();
+            //Route Checks -> Add widget and format multiple pages
+            if (window.location.pathname == '/index.html' ||
+                window.location.pathname == '/' ||
+                window.location.pathname == '/RandomWebBits/index.html' ||
+                window.location.pathname == '/RandomWebBits/') {
+                const getMultipleRandom = (arr, num) => {
+                    const shuffled = [...arr].sort(() => 0.5 - Math.random());
 
-    //Map WebBits to a card, each
-    //Map is fulcrum to create a WebBit card
-    
-    const buildWebBitsArticleCards = () => {
-        let AAs = ArbitraryArticles.map((article) => {
-            let WebBit = document.createElement('div');
-            WebBit.classList.add('card');
-            let cardImgTop = document.createElement('div');
-            let cardImg = document.createElement('img');
-            cardImg.setAttribute('src', article.cardImage);
-            cardImg.setAttribute('alt', article.cardImageALT);
-            cardImg.setAttribute('Article', article.articleNumber);
-            cardImgTop.appendChild(cardImg);
-            let cardBody = document.createElement('div');
-            cardBody.classList.add("cardBody");
-            let cardBodyHeading = document.createElement('h3');
-            let cardBodyPara = document.createElement('p');
-            let cardBodyLink = document.createElement('a');
-            cardBody.appendChild(cardBodyHeading);
-            cardBody.appendChild(cardBodyPara);
-            cardBody.appendChild(cardBodyLink);
-            cardBodyHeading.innerText = article.name;
-            FLATICONS.map((link) => {
-                if (cardImg.getAttribute('Article') == link.id){ //match WebBit ID to Icon ID
-                cardImgTop.classList.add("flip-card")
-                const cardInner = cardImgTop.appendChild(document.createElement("div"));
-                cardInner.classList.add("inner");
-                const cardFront = cardInner.appendChild(document.createElement("div"));
-                cardFront.classList.add("cardFront");
-                cardFront.appendChild(cardImg);
-                let smallImg = cardImg.cloneNode(false);
-                smallImg.classList.add("imgSmall");
-                const cardBack = cardInner.appendChild(document.createElement("div"));
-                cardBack.classList.add("cardBack");
-                const backHeading = cardBack.appendChild(document.createElement("h3"));
-                backHeading.textContent = link.owner;
-                cardBack.appendChild(smallImg);
-                const backPara = cardBack.appendChild(document.createElement("p"));
-                backPara.textContent = link.innerText
-                const attributeLink = cardBody.appendChild(document.createElement("a"));
-                attributeLink.href = link.hReference;
-                attributeLink.target = link.title;
-                attributeLink.textContent = link.owner;
-                attributeLink.classList.add("attribute");
+                    return shuffled.slice(0, num);
                 }
-            });
-            cardBodyPara.textContent = article.description;
-            cardBodyLink.setAttribute('href', article.articleLink)
-            cardBodyLink.textContent = "Go to Page";
 
-            WebBit.appendChild(cardImgTop);
-            WebBit.appendChild(cardBody);
-            return WebBit;
-        })
-        return AAs;
-    }
-
-    let WebBits = buildWebBitsArticleCards();
-
-    if(window.location.pathname == '/index.html' || 
-       window.location.pathname == '/' || 
-       window.location.pathname == '/RandomWebBits/index.html' || 
-       window.location.pathname == '/RandomWebBits/'){
-        const getMultipleRandom = (arr, num) => {
-            const shuffled = [...arr].sort(() => 0.5 - Math.random());
-        
-            return shuffled.slice(0, num);
+                WebBits = getMultipleRandom(WebBits, 3);
             }
-        
-        WebBits = getMultipleRandom(WebBits, 3);
-    }
-    
-    //from AA map, append each to section
-    WebBits.forEach((article) => {
-        aaCardsSection.append(article);
-    })
-})();
 
-            
+            //from AA map, append each to section
+            WebBits.forEach((article) => {
+                cardsSection.append(article);
+            })
+        },
+        buildCardSection: () => {
+            //Create Artibrary Articles section element and append to Main
+            const pageMain = document.querySelector("main");
+            const AASection = document.createElement("section");
+            AASection.classList.add("cards");
+            AASection.classList.add("dictionaryWidget");
+            AASection.classList.add("ToDoList");
+
+            //Create card section heading and div element. Append to section
+            let aaHeading = document.createElement('h2');
+            aaHeading.innerText = `Arbitrary Articles:`;
+            let aaCardsSection = document.createElement('div');
+            aaCardsSection.classList.add('card_columns');
+            AASection.appendChild(aaHeading);
+            AASection.appendChild(aaCardsSection);
+            pageMain.append(AASection);
+
+            return aaCardsSection;
+        },
+        buildWebBitsArticleCards: () => {
+            //Map WebBits to a card, each
+
+            let AAs = ArbitraryArticles.map((article) => {
+                let WebBit = document.createElement('div');
+                WebBit.classList.add('card');
+                let cardImgTop = document.createElement('div');
+                let cardImg = document.createElement('img');
+                cardImg.setAttribute('src', article.cardImage);
+                cardImg.setAttribute('alt', article.cardImageALT);
+                cardImg.setAttribute('Article', article.articleNumber);
+                cardImgTop.appendChild(cardImg);
+                let cardBody = document.createElement('div');
+                cardBody.classList.add("cardBody");
+                let cardBodyHeading = document.createElement('h3');
+                let cardBodyPara = document.createElement('p');
+                let cardBodyLink = document.createElement('a');
+                cardBody.appendChild(cardBodyHeading);
+                cardBody.appendChild(cardBodyPara);
+                cardBody.appendChild(cardBodyLink);
+                cardBodyHeading.innerText = article.name;
+                FLATICONS.map((link) => {
+                    //Determine if card image needs attribution panel
+                    if (cardImg.getAttribute('Article') == link.id) { //match WebBit ID to Icon ID
+                        cardImgTop.classList.add("flip-card")
+                        const cardInner = cardImgTop.appendChild(document.createElement("div"));
+                        cardInner.classList.add("inner");
+                        const cardFront = cardInner.appendChild(document.createElement("div"));
+                        cardFront.classList.add("cardFront");
+                        cardFront.appendChild(cardImg);
+                        let smallImg = cardImg.cloneNode(false);
+                        smallImg.classList.add("imgSmall");
+                        const cardBack = cardInner.appendChild(document.createElement("div"));
+                        cardBack.classList.add("cardBack");
+                        const backHeading = cardBack.appendChild(document.createElement("h3"));
+                        backHeading.textContent = link.owner;
+                        cardBack.appendChild(smallImg);
+                        const backPara = cardBack.appendChild(document.createElement("p"));
+                        backPara.textContent = link.innerText
+                        const attributeLink = cardBody.appendChild(document.createElement("a"));
+                        attributeLink.href = link.hReference;
+                        attributeLink.target = link.title;
+                        attributeLink.textContent = link.owner;
+                        attributeLink.classList.add("attribute");
+                    }
+                });
+                cardBodyPara.textContent = article.description;
+                cardBodyLink.setAttribute('href', article.articleLink)
+                cardBodyLink.textContent = "Go to Page";
+
+                WebBit.appendChild(cardImgTop);
+                WebBit.appendChild(cardBody);
+                return WebBit;
+            })
+            return AAs;
+        }
+    }
+})();

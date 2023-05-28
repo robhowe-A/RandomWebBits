f1ab2589cf9a14d8185de51d7ac877f7e1fcead2
rhowell476 <rhowell91@live.com>
2023-05-27 19:31:45 -0500

    abbr update

--- src/main.ts
+++ src/main.ts
@@ -9,62 +9,123 @@ import flashcardgameWidget from './components/FlashcardGameWidget';
 import slideshowWidget from './components/SlideShowWidget';
 
 // entry point
-(() => {
-    //PERF: let t1 = performance.now();
-    // Event fired before assets are rendered to the page
-    window.addEventListener("DOMContentLoaded", () => {
+/**
+ * TypeScript entry point. This script initializes page components and models as
+ *  they're needed main.init() is the initialization of "typescript.js".
+ */
+const main = {
+    /**
+     * Initialize page widgets and application functions.
+     */
+    init() {
+        //PERF: let t1 = performance.now();
+        // Event fired before assets are rendered to the page
+        window.addEventListener("DOMContentLoaded", () => {
 
-        //'Index' and 'Pages' routes, add cards widget
-        if (
-            window.location.pathname == '/RandomWebBits/index.html' ||
-            window.location.pathname == '/index.html' ||
-            window.location.pathname == '/' ||
-            window.location.pathname == '' ||
-            window.location.pathname == '/RandomWebBits/pages.html' ||
-            window.location.pathname == '/pages.html') {
-            RWBCardsWidget.init(); // cards widget initialization
+            //'Index' and 'Pages' routes, add cards widget
+            if (
+                window.location.pathname == '/RandomWebBits/index.html' ||
+                window.location.pathname == '/index.html' ||
+                window.location.pathname == '/' ||
+                window.location.pathname == '' ||
+                window.location.pathname == '/RandomWebBits/pages.html' ||
+                window.location.pathname == '/pages.html') {
+                RWBCardsWidget.init(); // cards widget initialization
+            }
+
+            // Add header and footer components
+            HeaderFooter.headerWidget.init();
+            HeaderFooter.footerWidget.init();
+
+            // Initialize page components
+            // dom.html page uses expandingLists component
+            if (window.location.pathname == '/pages/dom.html' ||
+                window.location.pathname == '/pages/svg.html') {
+                ExpandingListDOMWidget.init();
+            }
+
+            // Initialize webIDE widget
+            if (window.location.pathname == '/pages/webides.html') {
+                ActiveCardsWidget.init();
+            }
+
+            // Initialize webIDE page components
+            if (window.location.pathname == '/flashcards.html') {
+                flashcardgameWidget.init();
+            }
+            
+            // Initialize slideshow components
+            if (window.location.pathname == '/guides/pwaicon.html'){
+                slideshowWidget.init();
+            }
+
+            // Add dictionary widget if that class is on a page
+            const dictionaryElement = document.querySelector(".dictionaryWidget");
+            if (dictionaryElement) {
+                DictionaryWidget.init(dictionaryElement);
+            }
+
+            // Add ToDos widget if that class is on a page
+            const toDosElement = document.querySelector(".ToDoList");
+            if (toDosElement != null)
+                ToDosWidget.init(toDosElement);
+
+            //PERF: let t2 = performance.now();
+            //PERF: const totaltime = t2 - t1;
+            //PERF: console.log("the time is: ", totaltime);
+            main.mobileAbbrMarkup();
+        })
+    },
+    /**
+     * Attribute tags on mobile do not have hover option. This function adds a click
+     *  ability to define an abbr tag, than rely on the title attribute.
+     */
+    mobileAbbrMarkup() {
+        /**
+         * 
+         */
+        class AbbrOpen{
+            isOpen: boolean = false;
+            abbrElement: HTMLElement;
+            
+
+            constructor(){
+                this.isOpen = true;
+            };
         }
+        const allabbreviationelems = document.querySelectorAll("abbr");
+        if(allabbreviationelems.length > 0){
+            console.log(allabbreviationelems)//TODO: Delete else block, it's for logging
+            for (let abbr of allabbreviationelems){
+                let abbrev = new AbbrOpen();
+                abbrev.abbrElement = abbr;
 
-        // Add header and footer components
-        HeaderFooter.headerWidget.init();
-        HeaderFooter.footerWidget.init();
+                abbrev.abbrElement.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    let abbrtitleattrval:string = abbrev.abbrElement.getAttribute("title");
+                    let description: HTMLSpanElement;
 
-        // Initialize page components
-        // dom.html page uses expandingLists component
-        if (window.location.pathname == '/pages/dom.html' ||
-            window.location.pathname == '/pages/svg.html') {
-            ExpandingListDOMWidget.init();
+                    if (e.target == abbr){
+                        if(abbrev.abbrElement.children.length < 1){ //create the span element
+                            description = abbrev.abbrElement.appendChild(document.createElement("span"));
+                            description.textContent = `${String.fromCharCode(160)}(${abbrtitleattrval}${String.fromCharCode(160)})`;
+                        }                        
+                        else { //show the span element
+                            description = abbrev.abbrElement.querySelector("span");
+                            description.textContent = `${String.fromCharCode(160)}(${abbrtitleattrval}${String.fromCharCode(160)})`;
+                        }
+                    }
+                    abbrev.abbrElement.addEventListener("mouseleave", () => {
+
+                            description.textContent = "";
+                    });
+                });
+            }
         }
-
-        // Initialize webIDE widget
-        if (window.location.pathname == '/pages/webides.html') {
-            ActiveCardsWidget.init();
+        else { //TODO: Delete else block, it's for logging
+            console.log("%cNo abbr elements.", "color: green");
         }
+    }
+};
 
-        // Initialize webIDE page components
-        if (window.location.pathname == '/flashcards.html') {
-            flashcardgameWidget.init();
-        }
-        
-        // Initialize slideshow components
-        if (window.location.pathname == '/guides/pwaicon.html'){
-            slideshowWidget.init();
-        }
-
-        // Add dictionary widget if that class is on a page
-        const dictionaryElement = document.querySelector(".dictionaryWidget");
-        if (dictionaryElement) {
-            DictionaryWidget.init(dictionaryElement);
-        }
-
-        // Add ToDos widget if that class is on a page
-        const toDosElement = document.querySelector(".ToDoList");
-        if (toDosElement != null)
-            ToDosWidget.init(toDosElement);
-
-        //PERF: let t2 = performance.now();
-        //PERF: const totaltime = t2 - t1;
-        //PERF: console.log("the time is: ", totaltime);
-    })
-
-})();
+main.init();

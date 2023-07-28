ab9be8a577863210e9519a342704ce4c0f4dcea1
Robert A Howell <rhowell91@live.com>
2023-07-28 14:36:13 +0000

    1.3.56, CSS Widget

--- src/components/cssex.ts
+++ src/components/cssex.ts
@@ -1,35 +1,21 @@
 //--Copyright (c) 2023 Robert A. Howell
 
 const cssex = {
+    /**
+     * Cssex is a widget in CSS page, applying style colors to elements of different
+     * types (based on the CSS programming language)
+     */
     CSSEXColorCode: () => {
         const selectors = document.querySelectorAll(".Selector") as NodeListOf<HTMLElement>;
         const attributes = document.querySelectorAll(".Attribute") as NodeListOf<HTMLElement>;
         const values = document.querySelectorAll(".Value") as NodeListOf<HTMLElement>;
         const psuedos = document.querySelectorAll(".Psuedo-class") as NodeListOf<HTMLElement>;
         const resetBTN = document.querySelector(".reset");
-
-        // Function to color the example area's css code properties
-        const CSSExampleHighlighting = (elems: NodeListOf<HTMLElement>, color: string) =>{
-
-            elems.forEach((elem)=>{
-                elem.addEventListener("mouseover", (event)=>{
-                    event.preventDefault();
-                    elems.forEach((elem)=>{
-                        elem.style.color = color;
-                    });
-                })
-                elem.addEventListener("click", (event)=>{
-                    event.preventDefault();
-                    elems.forEach((elem)=>{
-                        elem.style.color = "initial";
-                    });
-                })
-            })
-        }
-        CSSExampleHighlighting(selectors, "var(--clr-Red)");
-        CSSExampleHighlighting(attributes, "var(--clr-WhoIS_Orange)");
-        CSSExampleHighlighting(values, "var(--clr-Skyblue)");
-        CSSExampleHighlighting(psuedos, "var(--clr-Green)");
+        
+        cssex.CSSExampleHighlighting(selectors, "var(--clr-Red)");
+        cssex.CSSExampleHighlighting(attributes, "var(--clr-WhoIS_Orange)");
+        cssex.CSSExampleHighlighting(values, "var(--clr-Skyblue)");
+        cssex.CSSExampleHighlighting(psuedos, "var(--clr-Green)");
         
         //function to reset the css code properties color to original
         resetBTN.addEventListener("click", ()=>{
@@ -45,6 +31,29 @@ const cssex = {
             psuedos.forEach((psuedo)=>{
                 psuedo.style.color = "initial";
             });
+        })
+    },
+    // 
+    /**
+     * Function to color the example area's css code properties
+     * @param elems - Node list of HTMLElelements from query.SelectorAll()
+     * @param color - String of CSS color value
+     */
+    CSSExampleHighlighting: (elems: NodeListOf<HTMLElement>, color: string) => {
+
+        elems.forEach((elem)=>{
+            elem.addEventListener("mouseover", (event)=>{
+                event.preventDefault();
+                elems.forEach((elem)=>{
+                    elem.style.color = color;
+                });
+            })
+            elem.addEventListener("click", (event)=>{
+                event.preventDefault();
+                elems.forEach((elem)=>{
+                    elem.style.color = "initial";
+                });
+            })
         })
     }
 }

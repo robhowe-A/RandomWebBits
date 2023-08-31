94cb5ffdffe1d38b38e8549e7e552b7e1e3b0736
Robert A Howell <rhowell91@live.com>
2023-08-31 14:28:54 +0000

    image finder

--- scripts/test.py
+++ scripts/test.py
@@ -1,61 +1,59 @@
 import os
 
-mypath = 'dist/img'
-imgfilenames = []
-
 def main():
+    mypath = 'dist/img'
+    imgfilenames = []
+    txtcount = 0
     jpegcount = 0
     jpgcount = 0
     svgcount = 0
     pngcount = 0
     webpcount = 0
-    filecounts = [jpegcount, jpgcount, svgcount, pngcount, webpcount]
+    filecounts = [txtcount, jpegcount, jpgcount, svgcount, pngcount, webpcount]
 
     def extswitch(extension):
         match extension:
             case ".txt":
-                
-                print(extension)
+                filecounts[0] += 1
                 return False
             case ".jpeg":
-                filecounts[0] += 1
-                print(extension)
+                filecounts[1] += 1
                 return True
             case ".jpg":
-                filecounts[1] += 1
-                print(extension)
+                filecounts[2] += 1
                 return True
             case ".svg":
-                filecounts[2] += 1
-                print(extension)
+                filecounts[3] += 1
                 return True
             case ".png":
-                filecounts[3] += 1
-                print(extension)
+                filecounts[4] += 1
                 return True
             case ".webp":
-                filecounts[4] += 1
-                print(extension)
+                filecounts[5] += 1
                 return True
 
 
     # traverse root directory, and list directories as dirs and files as files
-    for (root, dirs, files) in os.walk(mypath, topdown=True):
-        for file in files:
-            ext = os.path.splitext(file)[-1].lower()
-            keeptest = extswitch(ext)
-            if not keeptest:
-                break
-            imgfilenames.append(file)
+    for root, dirs, files in os.walk(mypath, topdown=False):
+        imgfilenames.extend(files)
+            
+    # filter files list of any .txt files            
+    imgfilenamescpy = imgfilenames
+    for file in imgfilenamescpy:
+        ext = os.path.splitext(file)[-1].lower()
+        keeptest = extswitch(ext)
+        if not keeptest:
+            imgfilenamescpy.remove(file)
 
 
     print("++++++++++++++++++++++++++++++++++++++++")
-    print("Total files in image folder: %i"%len(imgfilenames))
-    print("Total jpeg images: %i"%filecounts[0])
-    print("Total jpg images: %i"%filecounts[1])
-    print("Total svg images: %i"%filecounts[2])
-    print("Total png images: %i"%filecounts[3])
-    print("Total webp images: %i"%filecounts[4])
+    print("Total images in folder: %i"%len(imgfilenamescpy))
+    print("Total txt files: %i"%filecounts[0])
+    print("Total jpeg images: %i"%filecounts[1])
+    print("Total jpg images: %i"%filecounts[2])
+    print("Total svg images: %i"%filecounts[3])
+    print("Total png images: %i"%filecounts[4])
+    print("Total webp images: %i"%filecounts[5])
 
 
     print()

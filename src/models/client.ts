95d49ee624733d1dc6296f5d99e95fa19b1805c2
Robert A Howell <rhowell91@live.com>
2023-09-13 00:22:02 +0000

    version 1.7.72

--- src/models/client.ts
+++ src/models/client.ts
@@ -2,28 +2,47 @@
 
 export class client{
     oldURL = document.referrer;
-    browserplatform = this.setbrowserplatform();
+    browserplatform: string;
     useragent = window.navigator.userAgent;
-    connectiontype = this.setconnectiontype();
-    connectionrtt = this.setconnectionrtt();
+    connectiontype;
+    connectionrtt;
+
+    constructor(){
+        this.browserplatform = this.setbrowserplatform();
+        this.connectiontype = this.setconnectiontype();
+        this.connectionrtt = this.setconnectionrtt();
+    }
 
     setbrowserplatform() {
-        if ("userAgentData" in window.navigator)
-            return window.navigator.userAgentData.platform;
+        if ("userAgentData" in window.navigator){
+            //userAgentData is NavigatorUAData type, not found in TypeScript. 
+            //Known to Edge browser: Object.getPrototypeOf(window.navigator.userAgentData)
+            let userAgentData: any = window.navigator.userAgentData as object;
+            let platformdata: string = <string>userAgentData.platform;
+            return platformdata;
+        }
         else
             this.browserplatform = "";
     }
 
     setconnectiontype() {
-        if ("connection" in window.navigator)
-            return window.navigator.connection.effectiveType;
+        if ("connection" in window.navigator){
+            //connection is NetworkInformation type, not found in TypeScript.
+            //Known to Edge browser: Object.getPrototypeOf(window.navigator.connection)
+            let connection:any = window.navigator.connection as object
+            let effectivetype:string = <string>connection.effectiveType;
+            return effectivetype;
+        }
         else
             this.connectiontype = "";
     }
 
     setconnectionrtt() {
-        if ("connection" in window.navigator)
-            return window.navigator.connection.rtt;
+        if ("connection" in window.navigator){
+            let connection:any = window.navigator.connection as object
+            let rtt:string = <string>connection.rtt;
+            return rtt;
+        }
         else
             this.connectionrtt = "";
     }

e31151fb00bcda23cb57baddc5c258cb1e412aa8
Robert A Howell <rhowell91@live.com>
2023-09-27 19:28:45 +0000

    version 1.8.76

--- src/components/WebBitsSlideshow.ts
+++ src/components/WebBitsSlideshow.ts
@@ -5,7 +5,7 @@ const WebBitsSlideShow = {
     init: () => {
         if (window.location.pathname == '/pages.html') return;
         //var tablet = window.matchMedia("(min-width: 501px) and (max-width: 768px)");
-        var small = window.matchMedia("(max-width: 500px)");
+        var small = window.matchMedia("(max-width: 819px)");
         var tablet = window.matchMedia("(min-width: 820px) and (max-width: 992px)");
 
         var windowsize:string = "";

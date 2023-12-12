3d331b21e920131fad983b9c9e8cd85af4665aa2
Robert A Howell <rhowell91@live.com>
2023-12-12 01:09:00 +0000

    accordion

--- src/components/WebBitsAccordian.ts
+++ src/components/WebBitsAccordian.ts
@@ -19,16 +19,21 @@ const WebBitsAccordion = {
 
     let accordion = Object.create(new Accordion(cardaccordion));
 
-    if (window.matchMedia("(min-width: 769px)").matches) {
-      WebBitsAccordion.addCardAccordionStyles(accordion);
+    if (window.matchMedia("(min-width: 501px) and (max-width: 768px)").matches) {
+      WebBitsAccordion.addCardAccordionStyles(accordion, "SMALL");
+    } else if (window.matchMedia("(min-width: 769px)").matches) {
+      WebBitsAccordion.addCardAccordionStyles(accordion, "MEDIUM");
     }
 
     window.addEventListener("resize", e => {
       e.preventDefault();
-      if (window.matchMedia("(min-width: 769px)").matches) {
-        WebBitsAccordion.addCardAccordionStyles(accordion);
+      if (window.matchMedia("(min-width: 501px) and (max-width: 768px)").matches) {
+        WebBitsAccordion.addCardAccordionStyles(accordion, "SMALL");
       }
-      if (window.matchMedia("(max-width: 768px)").matches) {
+      if (window.matchMedia("(min-width: 769px)").matches) {
+        WebBitsAccordion.addCardAccordionStyles(accordion, "MEDIUM");
+      }
+      if (window.matchMedia("(max-width: 500px)").matches) {
         WebBitsAccordion.removeCardAccorionStyles(accordion);
       }
     });
@@ -37,16 +42,16 @@ const WebBitsAccordion = {
     //add styles for tablet screen
     //add styles for desktop screen
   },
-  addCardAccordionStyles: (cardaccordion: Accordion) => {
-    cardaccordion.accordionElements.forEach((val, key) => {
-      key.style.removeProperty("flexDirection");
-      key.style.setProperty("max-width", "900px");
-      key.style.setProperty("margin", "0");
-      key.style.setProperty("height", "100px");
-      key.style.setProperty("overflow", "hidden");
+  addCardAccordionStyles: (cardaccordion: Accordion, screenSize: string) => {
+    cardaccordion.accordionElements.forEach((isOpen, card) => {
+      card.style.removeProperty("flexDirection");
+      card.style.setProperty("max-width", "900px");
+      card.style.setProperty("margin", "0");
+      card.style.setProperty("height", "100px");
+      card.style.setProperty("overflow", "hidden");
       //hide the 'Flaticon' links
 
-      for (let inner of key.childNodes) {
+      for (let inner of card.childNodes) {
         let innerelem = <HTMLElement>inner;
         innerelem.style.width = "50%";
 
@@ -54,39 +59,68 @@ const WebBitsAccordion = {
         // flaticonelem.style.right = "50%";
         // flaticonelem.style.top = "50%";
       }
-      let attrlink = key.childNodes[1].childNodes[3] as HTMLAnchorElement;
+      //card body attr style
+      let attrlink = card.childNodes[1].childNodes[3] as HTMLAnchorElement;
       attrlink.style.top = "55px";
       attrlink.style.right = "100%";
 
+      if (screenSize == "SMALL") {
+        //card image style
+        let image = card.childNodes[0].childNodes[0].childNodes[0].childNodes[0] as HTMLImageElement;
+        image.style.setProperty("max-height", "200px");
+        image.style.setProperty("max-width", "200px");
+
+        //card image small style
+        let imageSmall = card.childNodes[0].childNodes[0].childNodes[1].childNodes[1] as HTMLImageElement;
+        imageSmall.style.setProperty("max-height", "100px");
+        imageSmall.style.setProperty("max-width", "100px");
+
+        //card back para style
+        let imagePara = card.childNodes[0].childNodes[0].childNodes[1].childNodes[2] as HTMLImageElement;
+        imagePara.style.setProperty("margin", "0");
+        imagePara.style.setProperty("font-size", "16px");
+      }
+      if (screenSize == "MEDIUM") {
+        //card image style
+        let image = card.childNodes[0].childNodes[0].childNodes[0].childNodes[0] as HTMLImageElement;
+        image.style.setProperty("max-height", "275px");
+        image.style.setProperty("max-width", "275px");
+
+        //card image small style
+        let imageSmall = card.childNodes[0].childNodes[0].childNodes[1].childNodes[1] as HTMLImageElement;
+        imageSmall.style.setProperty("max-height", "100px");
+        imageSmall.style.setProperty("max-width", "100px");
+      }
+
       //add click event
-      key.addEventListener("click", e => {
+      card.addEventListener("click", e => {
         e.preventDefault();
         const close = () => {
-          key.style.setProperty("height", "100px");
-          val = false;
+          card.style.setProperty("height", "100px");
+          isOpen = false;
         };
 
         const open = () => {
-          key.style.setProperty("height", "350px");
-          val = true;
+          card.style.setProperty("height", "275px");
+          isOpen = true;
         };
-        val ? close() : open();
+        isOpen ? close() : open();
       });
       //add focus event
-      let sitelink = key.childNodes[1].childNodes[2] as HTMLAnchorElement;
+      let sitelink = card.childNodes[1].childNodes[2] as HTMLAnchorElement;
       sitelink.addEventListener("focus", e => {
         e.preventDefault();
         const open = () => {
-          key.style.setProperty("height", "350px");
-          val = true;
+          card.style.setProperty("height", "350px");
+          isOpen = true;
         };
         open();
       });
       sitelink.addEventListener("focusout", e => {
         e.preventDefault();
         const close = () => {
-          key.style.setProperty("height", "100px");
-          val = false;
+          card.style.setProperty("height", "100px");
+          isOpen = false;
         };
         close();
       });
@@ -94,8 +128,8 @@ const WebBitsAccordion = {
       attrlink.addEventListener("focusout", e => {
         e.preventDefault();
         const close = () => {
-          key.style.setProperty("height", "100px");
-          val = false;
+          card.style.setProperty("height", "100px");
+          isOpen = false;
         };
         close();
       });
@@ -103,8 +137,8 @@ const WebBitsAccordion = {
       attrlink.addEventListener("focus", e => {
         e.preventDefault();
         const open = () => {
-          key.style.setProperty("height", "350px");
-          val = true;
+          card.style.setProperty("height", "350px");
+          isOpen = true;
         };
         open();
       });
@@ -112,21 +146,35 @@ const WebBitsAccordion = {
   },
 
   removeCardAccorionStyles: (cardaccordion: Accordion) => {
-    cardaccordion.accordionElements.forEach((val, key) => {
-      key.style.setProperty("flexDirection", "column");
-      key.style.removeProperty("height");
+    cardaccordion.accordionElements.forEach((isOpen, card) => {
+      card.style.setProperty("flexDirection", "column");
+      card.style.removeProperty("height");
 
-      key.style.removeProperty("max-width");
-      key.style.removeProperty("margin");
-      key.style.removeProperty("overflow");
-      key.removeAttribute("style");
+      card.style.removeProperty("max-width");
+      card.style.removeProperty("margin");
+      card.style.removeProperty("overflow");
+      card.removeAttribute("style");
 
-      let attrlink = key.childNodes[1].childNodes[3] as HTMLAnchorElement;
+      //card body attr style
+      let attrlink = card.childNodes[1].childNodes[3] as HTMLAnchorElement;
       attrlink.style.removeProperty("top");
       attrlink.style.removeProperty("right");
       attrlink.removeAttribute("style");
 
-      for (let inner of key.childNodes) {
+      //card image style
+      let image = card.childNodes[0].childNodes[0].childNodes[0].childNodes[0] as HTMLImageElement;
+      image.style.removeProperty("max-height");
+
+      //card image small style
+      let imageSmall = card.childNodes[0].childNodes[0].childNodes[1].childNodes[1] as HTMLImageElement;
+      imageSmall.style.removeProperty("max-height");
+
+      //card back para style
+      let imagePara = card.childNodes[0].childNodes[0].childNodes[1].childNodes[2] as HTMLImageElement;
+      imagePara.style.removeProperty("margin");
+      imagePara.style.removeProperty("font-size");
+
+      for (let inner of card.childNodes) {
         let innerelem = <HTMLElement>inner;
         innerelem.style.removeProperty("width");
         innerelem.removeAttribute("style");
